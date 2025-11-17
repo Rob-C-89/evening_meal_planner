66adab0476f456a6c38194bfb9fade594a7d4f27
Rob-C-89 <robbiecornell@gmail.com>
2025-11-17 10:00:30 +0000

    Delete gspread and pandas

--- run.py
+++ run.py
@@ -1,17 +1,2 @@
-import gspread
-from google.oauth2.service_account import Credentials
-
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('evening_meal_planner')
-
-print("Welcome to your Evening Meal Planner!")
-print("Here is your week's meal plan:")
-
+print("Welcome to your Evening Meal Planner!\n")
+print("Here is your week's meal plan:\n")
